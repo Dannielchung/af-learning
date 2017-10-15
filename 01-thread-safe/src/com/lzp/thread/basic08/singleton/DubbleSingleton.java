228986a26967fbbb79d0920a234cdaf0e9eb6a9f
lzp <lzp9294@163.com>
2017-10-15 20:09:44 +0800

    线程池

--- 01-thread-safe/src/com/lzp/thread/basic08/singleton/DubbleSingleton.java
+++ 01-thread-safe/src/com/lzp/thread/basic08/singleton/DubbleSingleton.java
@@ -2,16 +2,12 @@ package com.lzp.thread.basic08.singleton;
 
 public class DubbleSingleton {
 
-	private static DubbleSingleton ds;
+	private volatile static DubbleSingleton ds;
+
+	private DubbleSingleton(){}
 	
-	public static DubbleSingleton getDs(){
+	public static DubbleSingleton getIntance(){
 		if(ds == null){
-			try {
-				//模拟初始化对象的准备时间...
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 			synchronized (DubbleSingleton.class) {
 				if(ds == null){
 					ds = new DubbleSingleton();
@@ -19,31 +15,6 @@ public class DubbleSingleton {
 			}
 		}
 		return ds;
-	}
-	
-	public static void main(String[] args) {
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println(DubbleSingleton.getDs().hashCode());
-			}
-		},"t1");
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println(DubbleSingleton.getDs().hashCode());
-			}
-		},"t2");
-		Thread t3 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println(DubbleSingleton.getDs().hashCode());
-			}
-		},"t3");
-		
-		t1.start();
-		t2.start();
-		t3.start();
 	}
 	
 }
